8e83807bd701f813836a3a29700b9eb3b0363be9
SandroRDS <sandror.desouza847@gmail.com>
2023-09-24 17:13:24 -0300

    Removendo backgorund e chao

--- config/configGround.js
+++ config/configGround.js
@@ -1,11 +1,4 @@
 function configGround() {
-  const ground = new GameObject('ground', [
-    '/assets/ground/ground-1.png',
-    '/assets/ground/ground-2.png',
-    '/assets/ground/ground-3.png',
-    '/assets/ground/ground-4.png',
-  ], 1);
-
-  globalThis.ground = ground;
+  
 }
 
